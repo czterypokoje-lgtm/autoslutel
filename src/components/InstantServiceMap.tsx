8d18a3ab1e7d33d2f87de3db916ffffdfe73fbfe
Mac <mac@172-0-40-55.lightspeed.ltrkar.sbcglobal.net>
2026-07-14 01:09:02 +0200

    fix: change map loading overlay to CSS background spinner to prevent load blockages on cached page-views

--- src/components/InstantServiceMap.tsx
+++ src/components/InstantServiceMap.tsx
@@ -1,27 +1,22 @@
 'use client';
 
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './InstantServiceMap.module.css';
 
 export default function InstantServiceMap() {
-  const [isLoading, setIsLoading] = useState(true);
-
   return (
     <div className={styles.mapRoot}>
       <div className={styles.iframeWrapper}>
-        {isLoading && (
-          <div className={styles.loadingSpinnerOverlay}>
-            <div className={styles.spinner}></div>
-            <span>Kaart laden...</span>
-          </div>
-        )}
+        <div className={styles.spinnerBehind}>
+          <div className={styles.spinner}></div>
+          <span>Kaart laden...</span>
+        </div>
         <iframe
           src="https://www.google.com/maps/d/embed?mid=1M3Pmk5vzguoPL4qS81XLU_gz5OiXDF4&ehbc=2E312F"
           className={styles.googleMapIframe}
           allowFullScreen
           referrerPolicy="no-referrer-when-downgrade"
           title="Autosleutel24 Mobiele Service Werkgebied en Servicegebieden Google My Maps"
-          onLoad={() => setIsLoading(false)}
         ></iframe>
       </div>
     </div>
