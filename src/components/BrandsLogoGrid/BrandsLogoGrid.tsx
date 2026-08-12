39103765e79d8480737e49fec0cc902c1fd7fbe7
f23 <ik@172-0-40-51.lightspeed.ltrkar.sbcglobal.net>
2026-08-12 22:51:04 +0200

    Hardcode 59 brands in BrandsLogoGrid link instead of relying on array length

--- src/components/BrandsLogoGrid/BrandsLogoGrid.tsx
+++ src/components/BrandsLogoGrid/BrandsLogoGrid.tsx
@@ -84,7 +84,7 @@ export default function BrandsLogoGrid({ title, subtitle, hideSeoHeader = false 
 
         <div style={{ textAlign: 'center', marginTop: '2.5rem' }}>
           <Link href="/merken" className={styles.brandsAllLink}>
-            {"Bekijk alle " + BRANDS_WITH_LOGOS.length + " merken die wij bedienen \u2192"}
+            Bekijk alle 59 merken die wij bedienen &rarr;
           </Link>
         </div>
       </div>
