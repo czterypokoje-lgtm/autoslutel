ec0c6da1757ea0528088d4edcf2d16e45543a510
f23 <ik@172-0-40-51.lightspeed.ltrkar.sbcglobal.net>
2026-08-23 04:31:01 +0200

    UI: Fix Google preferred source button cropping

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -83,7 +83,7 @@ export default function Footer() {
                   style={{ height: '60px', width: 'auto', display: 'block', borderRadius: '6px', border: '1px solid rgba(255,255,255,0.12)' }}
                 />
               </a>
-              <div style={{ marginTop: '0.75rem', display: 'inline-block', maxWidth: '280px', width: '100%' }}>
+              <div className={styles.googleBtnWrapper}>
                 <div {...{ 'google-add-preferred-source-btn': 'true' }} data-theme="dark" data-lang="nl" style={{ width: '100%' }}></div>
               </div>
             </div>
